24349915cc1310495a826022076e0197eca6f975
Fabio Oliveira <fabio20rj@gmail.com>
2020-10-28 08:28:49 -0300

    Adicionado view update.

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from core.views import home
+from core.views import home, criar_empresa, atualizar_empresa
 
-urlpatterns = [path("", home, name="home")]
+urlpatterns = [
+    path("", home, name="home"),
+    path("criar/", criar_empresa, name="criar_empresa"),
+    path("editar/<int:pk>", atualizar_empresa, name="atualizar_empresa"),
+]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+
 from core.models import Empresa
+
+from .forms import CriarEmpresaForm
 
 
 def home(request):
     empresas = Empresa.objects.all()
     return render(request, "index.html", {"empresas": empresas})
+
+
+def criar_empresa(request):
+    if request.method == "POST":
+        form = CriarEmpresaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = CriarEmpresaForm()
+
+    return render(request, "create.html", {"form": form})
+
+
+def atualizar_empresa(request, pk):
+    empresa = get_object_or_404(Empresa, pk=pk)
+    if request.method == "POST":
+        form = CriarEmpresaForm(data=request.POST, instance=empresa)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = CriarEmpresaForm(instance=empresa)
+
+    return render(request, "update.html", {"form": form})
