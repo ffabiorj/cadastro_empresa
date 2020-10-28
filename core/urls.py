3332963d6ea99ec2a2e2552a99a2967bf0a265f9
Fabio Oliveira <fabio20rj@gmail.com>
2020-10-28 08:48:38 -0300

    Adicionado url excluir.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from core.views import home, criar_empresa, atualizar_empresa
+from core.views import home, criar_empresa, atualizar_empresa, excluir_empresa
 
 urlpatterns = [
     path("", home, name="home"),
     path("criar/", criar_empresa, name="criar_empresa"),
     path("editar/<int:pk>", atualizar_empresa, name="atualizar_empresa"),
+    path("excluir/<int:pk>", excluir_empresa, name="excluir_empresa"),
 ]
