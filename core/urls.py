3f33bd4707665ccb1e58821f6d30b3821a2cddb3
Fabio Oliveira <fabio20rj@gmail.com>
2020-10-28 19:59:10 -0300

    Adicionado url visualizer .

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from core.views import home, criar_empresa, atualizar_empresa, excluir_empresa
+from core.views import (
+    home,
+    criar_empresa,
+    atualizar_empresa,
+    excluir_empresa,
+    visualizar_empresa,
+)
 
 urlpatterns = [
     path("", home, name="home"),
     path("criar/", criar_empresa, name="criar_empresa"),
+    path("vizualizar/<int:pk>", visualizar_empresa, name="visualizar_empresa"),
     path("editar/<int:pk>", atualizar_empresa, name="atualizar_empresa"),
     path("excluir/<int:pk>", excluir_empresa, name="excluir_empresa"),
 ]
