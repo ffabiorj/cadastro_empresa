7ff34849eb3603886ed77beb1cb93ff40aca8824
Fabio Oliveira <fabio20rj@gmail.com>
2020-10-29 12:44:05 -0300

    Atualizado o model.

--- core/models.py
+++ core/models.py
@@ -15,19 +15,51 @@ class Empresa(models.Model):
         ("H", "H"),
     ]
 
-    razao_social = models.CharField(max_length=200)
-    nome_fantasia = models.CharField(max_length=200)
-    cnpj = models.CharField(max_length=14, validators=[validate_cnpj])
-    email = models.EmailField()
-    endereco = models.CharField(max_length=100)
-    cidade = models.CharField(max_length=20)
-    estado = models.CharField(max_length=5)
-    telefone = models.CharField(max_length=15)
-    data_cadastro = models.DateField()
-    status = models.BooleanField(default=True)
-    risco = models.CharField(max_length=2, choices=RISCOS)
-    agencia = models.CharField(max_length=6)
-    conta = models.CharField(max_length=10)
+    STATUS = [("A", "Ativo"), ("I", "Inativo")]
+
+    ESTADO = [
+        ("AC", "AC"),
+        ("AL", "AL"),
+        ("AP", "AP"),
+        ("AM", "AM"),
+        ("BA", "BA"),
+        ("CE", "CE"),
+        ("ES", "ES"),
+        ("GO", "GO"),
+        ("MA", "MA"),
+        ("MT", "MT"),
+        ("MS", "MS"),
+        ("MG", "MG"),
+        ("PA", "PA"),
+        ("PB", "PB"),
+        ("PR", "PR"),
+        ("PE", "PE"),
+        ("PI", "PI"),
+        ("RJ", "RJ"),
+        ("RN", "RN"),
+        ("RS", "RS"),
+        ("RO", "RO"),
+        ("RR", "RR"),
+        ("SC", "SC"),
+        ("SP", "SP"),
+        ("SE", "SE"),
+        ("TO", "TO"),
+        ("DF", "DF"),
+    ]
+
+    razao_social = models.CharField(max_length=200, blank=False, null=False)
+    nome_fantasia = models.CharField(max_length=200, blank=True, null=True)
+    cnpj = models.CharField(max_length=19, blank=False, null=False)
+    email = models.EmailField(blank=True, null=True)
+    endereco = models.CharField(max_length=100, blank=True, null=True)
+    cidade = models.CharField(max_length=20, blank=True, null=True)
+    estado = models.CharField(max_length=2, choices=ESTADO, blank=True, null=True)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    data_cadastro = models.DateField(blank=True, null=True)
+    status = models.CharField(max_length=6, choices=STATUS, blank=True, null=True)
+    risco = models.CharField(max_length=2, choices=RISCOS, blank=True, null=True)
+    agencia = models.CharField(max_length=6, blank=True, null=True)
+    conta = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.nome_fantasia
