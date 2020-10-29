d4eb550824453ba2a0e478651856a722cf774a31
Fabio Oliveira <fabio20rj@gmail.com>
2020-10-29 19:51:31 -0300

    deletado validator

--- core/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_cnpj(value):
-    if not value.isdigit():
-        raise ValidationError("CNPJ deve conter apenas números.", "digits")
-    if len(value) != 14:
-        raise ValidationError("CNPJ deve ter 14 numeros.", "length")
