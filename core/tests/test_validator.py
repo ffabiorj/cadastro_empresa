a5ab4311bfe19d12e7246649d1eaaaa31f0d4520
Fabio Oliveira <fabio20rj@gmail.com>
2020-11-03 10:55:32 -0300

    Atualizado os test de validador.

--- core/tests/test_validator.py
+++ core/tests/test_validator.py
@@ -4,5 +4,14 @@ from django.core.validators import ValidationError
 
 
 class ValidatorTest(TestCase):
+    def test_validator_none(self):
+        self.assertIsNone(valid_cnpj("35379838000112"))
+
     def test_validator_invalid(self):
         self.assertRaises(ValidationError, valid_cnpj, "teste")
+
+    def test_validator_invalid_caracter(self):
+        self.assertRaises(ValidationError, valid_cnpj, "22*222$222/222*2-22")
+
+    def test_validator_len_more_then_14(self):
+        self.assertRaises(ValidationError, valid_cnpj, "222222222222222222")
